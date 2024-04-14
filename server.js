8273d8db052e66a2bda0ed07c3ce7ee475139022
Roshan <roshanprajapati9075@gmail.com>
2024-04-14 09:52:36 +0530

    po

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser')
